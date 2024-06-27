e38607c58bb9599f1f6dec97d31a54c4a953c316
Tikhon20043 <144627214+Tikhon20043@users.noreply.github.com>
2024-06-27 12:59:16 +0300

    .

--- ЕГЭ.py
+++ ЕГЭ.py
@@ -1,3 +1,7 @@
+#Текстовый файл состоит из символов I, U, V, W, X, U и Z.
+#Определите в прилагаемом файле максимальное количество идущих подряд символов (длину непрерывной подпоследовательности),
+#среди которых символ Х встречается не более 140 раз.
+
 def find_max_sequence(file_path):
     with open(file_path, 'r') as file:
         text = file.read()
@@ -7,7 +11,7 @@ def find_max_sequence(file_path):
     x_count = 0
 
     for char in text:
-        if char in ['I', 'U', 'V', 'W', 'X', 'U', 'Z']:
+        if char in ['T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
             current_length += 1
             if char == 'X':
                 x_count += 1
@@ -28,3 +32,4 @@ def find_max_sequence(file_path):
 
 file_path = '313_24.txt'
 print(find_max_sequence(file_path))
+
